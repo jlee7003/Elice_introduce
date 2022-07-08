4291f8f7e3ccbdf92ae12be8886663a3111e1a72
이 안토니의호 <jlee7003@naver.com>
2022-07-08 18:05:52 +0900

    first

---
+++ main.js
@@ -0,0 +1,55 @@
+const tabl = document.querySelector("#tab1");
+const tab2 = document.querySelector("#tab2");
+const tab3 = document.querySelector("#tab3");
+const tab4 = document.querySelector("#tab4");
+const tab5 = document.querySelector("#tab5");
+const tab6 = document.querySelector("#tab6");
+
+function showtabl() {
+  tab1.style.display = "block";
+  tab2.style.display = "none";
+  tab3.style.display = "none";
+  tab4.style.display = "none";
+  tab5.style.display = "none";
+  tab6.style.display = "none";
+}
+function showtab2() {
+  tab2.style.display = "block";
+  tab1.style.display = "none";
+  tab3.style.display = "none";
+  tab4.style.display = "none";
+  tab5.style.display = "none";
+  tab6.style.display = "none";
+}
+function showtab3() {
+  tab1.style.display = "none";
+  tab2.style.display = "none";
+  tab3.style.display = "block";
+  tab4.style.display = "none";
+  tab5.style.display = "none";
+  tab6.style.display = "none";
+}
+function showtab4() {
+  tab1.style.display = "none";
+  tab2.style.display = "none";
+  tab3.style.display = "none";
+  tab4.style.display = "block";
+  tab5.style.display = "none";
+  tab6.style.display = "none";
+}
+function showtab5() {
+  tab1.style.display = "none";
+  tab2.style.display = "none";
+  tab3.style.display = "none";
+  tab4.style.display = "none";
+  tab5.style.display = "block";
+  tab6.style.display = "none";
+}
+function showtab6() {
+  tab1.style.display = "none";
+  tab2.style.display = "none";
+  tab3.style.display = "none";
+  tab4.style.display = "none";
+  tab5.style.display = "none";
+  tab6.style.display = "block";
+}
